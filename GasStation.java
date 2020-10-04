737b7485549c6c1a1557890e2b9f4c62f0f9c4fe
souravaswal <souravaswal07@gmail.com>
2020-10-04 14:59:41 +0530

    Added one more question

---
+++ GasStation.java
@@ -0,0 +1,36 @@
+public class Solution {
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    public int canCompleteCircuit(final int[] A, final int[] B) {
+        for(int i=0; i<A.length; i++)
+        {
+            int amountOfGas = 0;
+            int gasNeeded = 0;
+            int length = 0;
+            int j = i;
+            
+            // As we are being greedy hence, we will return the first index which satisfies our need
+            while(length != B.length)
+            {
+                if(j >= B.length)
+                {
+                    j = j%B.length;
+                }
+                //Adding gas from each station & adding it in amountOfGas & check for each gas pump
+                amountOfGas+=A[j];
+                gasNeeded+=B[j];
+                if(amountOfGas < gasNeeded)
+                {
+                    break;
+                }
+                j++;
+                length++;
+            }
+            // Checks if amountOfGas was more than gasNeeded at the end of the loop.
+            if(amountOfGas >= gasNeeded)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
