6755daffae41ac6214ba9a830994e461d865ce2c
Longclaw <varunkashyap.vkd@gmail.com>
2022-10-09 18:13:13 +0530

    Updated comments in the code

--- 2Sum.java
+++ 2Sum.java
@@ -1,6 +1,6 @@
 /**
 
-Given an array of integers, find two numbers such that they add up to a specific target number
+Given an array of integers, find two numbers such that they add up to a specific target number.
 
 The function twoSum should return indices of the two numbers such that they add up to the target, where index1 < index2. Please note that your returned answers (both index1 and index2 ) are not zero-based.
 Put both these numbers in order in an array and return the array from your function (Looking at the function signature will make things clearer). Note that, if no pair exists, return empty list.
