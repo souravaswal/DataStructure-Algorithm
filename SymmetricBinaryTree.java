ee978a96ba4811134892cb20bba32c5a53bb2af1
shubg1995 <50797078+shubg1995@users.noreply.github.com>
2022-10-10 21:44:11 +0530

    Update SymmetricBinaryTree.java

--- SymmetricBinaryTree.java
+++ SymmetricBinaryTree.java
@@ -36,29 +36,22 @@ Return 0 / 1 ( 0 for false, 1 for true ) for this problem
  *     }
  * }
  */
-public class Solution 
-{
-    public int isSymmetric(TreeNode A) 
-    {
-        if(A == null)
-        {
+public class Solution {
+    public int isSymmetric(TreeNode A) {
+        if(A == null) {
             return 1;
         }
-        if((A.left == null && A.right != null) || (A.right == null && A.left != null))
-        {
+        if((A.left == null && A.right != null) || (A.right == null && A.left != null)) {
             return 0;
         }
         return checkSymmetric(A.left, A.right) ? 1 : 0;
     }
     
-    public boolean checkSymmetric(TreeNode A, TreeNode B)
-    {
-        if(A == null && B == null)
-        {
+    public boolean checkSymmetric(TreeNode A, TreeNode B) {
+        if(A == null && B == null) {
             return true;
         }
-        if(A == null || B == null)
-        {
+        if(A == null || B == null) {
             return false;
         }
         return A.val == B.val && checkSymmetric(A.left, B.right) && checkSymmetric(A.right, B.left);
