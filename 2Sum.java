d801078495a12bfc333c86b0573025bf5b471024
Sourav Aswal <souravaswal07@gmail.com>
2023-10-12 06:50:45 +0530

    Update 2Sum.java

--- 2Sum.java
+++ 2Sum.java
@@ -8,7 +8,7 @@ Put both these numbers in order in an array and return the array from your funct
 If multiple solutions exist, output the one where index2 is minimum. If there are multiple solutions with the minimum index2, choose the one with minimum index1 out of them.
 
 Input: [2, 7, 11, 15], target=9
-Output: index1 = 1, index2 = 2
+Output: index1 = 1, index2 = 2.
 
 **/
 
